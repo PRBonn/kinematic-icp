b1108d463ca042179d0286acdf200cb4134b2830
tizianoGuadagnino <frevo93@gmail.com>
2024-12-01 19:57:40 +0100

    Want to measure also the addPoints function

--- cpp/kinematic_icp/benchmark/voxel_grids_benchmark.cpp
+++ cpp/kinematic_icp/benchmark/voxel_grids_benchmark.cpp
@@ -36,6 +36,30 @@ static void GetClosestNeighbor_VoxelHashMap(benchmark::State &state) {
     }
 }
 
+static void AddPoints_BonxaiGrid(benchmark::State &state) {
+    World world;
+    kinematic_icp::pipeline::Config default_config;
+    const auto trajectory = world.generateCircularTrajectory();
+    const auto scan = world.Generate3DScan();
+    kinematic_icp::SparseVoxelGrid grid(default_config.voxel_size, default_config.max_range,
+                                        default_config.max_points_per_voxel);
+    for (auto _ : state) {
+        grid.AddPoints(scan);
+    }
+}
+
+static void AddPoints_VoxelHashMap(benchmark::State &state) {
+    World world;
+    kinematic_icp::pipeline::Config default_config;
+    const auto trajectory = world.generateCircularTrajectory();
+    const auto scan = world.Generate3DScan();
+    kiss_icp::VoxelHashMap grid(default_config.voxel_size, default_config.max_range,
+                                default_config.max_points_per_voxel);
+    for (auto _ : state) {
+        grid.AddPoints(scan);
+    }
+}
+
 static void Update_BonxaiGrid(benchmark::State &state) {
     World world;
     kinematic_icp::pipeline::Config default_config;
@@ -70,7 +94,9 @@ static void Update_VoxelHashMap(benchmark::State &state) {
 
 BENCHMARK(GetClosestNeighbor_BonxaiGrid);
 BENCHMARK(GetClosestNeighbor_VoxelHashMap);
-BENCHMARK(Update_BonxaiGrid);
+BENCHMARK(AddPoints_BonxaiGrid);
+BENCHMARK(AddPoints_BonxaiGrid);
+BENCHMARK(Update_VoxelHashMap);
 BENCHMARK(Update_VoxelHashMap);
 // Run the benchmark
 BENCHMARK_MAIN();
